50d7969b4b409060dd04697f22e291eb8b5bde19
xiaYuTian11 <38251546+xiaYuTian11@users.noreply.github.com>
2023-02-27 17:56:40 +0800

    完善

--- src/main/java/top/tanmw/oracle2dm/service/TransformService.java
+++ src/main/java/top/tanmw/oracle2dm/service/TransformService.java
@@ -152,7 +152,9 @@ public class TransformService {
                         int finalI = i;
                         String finalConstraint = constraint;
                         if (i != 0) {
-                            // ThreadUtil.sleep(60);
+                            ThreadUtil.sleep(60);
+                        }
+                        if (i >= 2) {
                             cd.countDown();
                             continue;
                         }
